1ad52b3cf0d911940d81503931104b8a15409adf
JC Izquierdo <josecarlosizquierdo1994@gmail.com>
2020-06-29 18:13:51 -0500

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localHost',
     dialect: 'mysql',
     port: 3306
