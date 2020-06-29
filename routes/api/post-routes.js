8b0a798fb0e6f26d709d93de5ec9f3e59e445a19
JC Izquierdo <josecarlosizquierdo1994@gmail.com>
2020-06-29 16:49:01 -0500

    feature/comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 
@@ -17,6 +17,14 @@ router.get('/', (req, res) => {
         ],
       order: [['created_at', 'DESC']], 
       include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+             include: {
+            model: User,
+            attributes: ['username']
+            },
+        },
         {
           model: User,
           attributes: ['username']
@@ -42,12 +50,20 @@ router.get('/', (req, res) => {
                     'created_at',
                     [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
                 ],
-      include: [
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
+                include: [
+                    {
+                      model: Comment,
+                      attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                       include: {
+                      model: User,
+                      attributes: ['username']
+                      },
+                  },
+                  {
+                    model: User,
+                    attributes: ['username']
+                  }
+                ]
     })
       .then(dbPostData => {
         if (!dbPostData) {
